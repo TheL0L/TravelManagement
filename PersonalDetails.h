f4bb7f707f57404a2808070aa9196eb2552ceb89
Sergei <sergeiyakima5@gmail.com>
2022-11-22 09:53:20 +0200

    added documentaion for PersonalDetails class fields and methods

--- PersonalDetails.h
+++ PersonalDetails.h
@@ -2,6 +2,27 @@
 #include <iostream>
 using namespace std;
 
+/*
+fields:
+    TravelCode - each travel entry has 'unique' id by which the details can be accessed
+    age[] - array of family member ages
+    num - number of kids in family
+    numppl - total number of family members (adults + kids)
+    Name[] - array of family member names
+    Address[] - string containing family address
+    PhoneNumber[] - string containing contact phone number for the family
+    gender[] - char array containing M/F as gender representaion of family members
+    PassportNum[] - array of strings containing passport numbers (ids) of family members
+    FamilyName[] - string representing the family name
+
+methods:
+    p_input(cd) - function gets the TravelCode as arg, and collects family details from input
+    p_output() - function prints family details
+    givefam() - function prints family name
+    givecode() - function returns TravelCode
+    givenum() - function returns the number of adults (total members - kids)
+*/
+
 
 class PersonalDetails // class for collecting the personal details
 {
