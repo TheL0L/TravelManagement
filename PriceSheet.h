47bfeac4646a58245f2bd10ea66a368fa59cbfea
Sergei <sergeiyakima5@gmail.com>
2022-12-16 20:58:06 +0200

    renamed ID field to match implementaion of FileManager

--- PriceSheet.h
+++ PriceSheet.h
@@ -4,7 +4,7 @@
 struct PriceSheet
 {
     /* prices are based on destination, each dest. has unique id. */
-    int destination_id;
+    int ID;
     int destination_ticket = 0, flight_class = 0, per_reserved_day = 0;
     int swimming_pool = 0, gym = 0, other_sports = 0;
     int spa = 0, beauty_salon = 0;
