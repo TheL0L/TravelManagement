ef46c6bffa78561a0254076cae9245da46dbbbf4
Sergei <sergeiyakima5@gmail.com>
2022-12-16 02:08:52 +0200

    Implemented currency management

---
+++ Currency.cpp
@@ -0,0 +1,91 @@
+#include "Currency.h"
+
+Currency::Currency()
+{
+    this->value = 0;
+    this->type = CurrencyType::USD;
+}
+
+Currency::Currency(float value, CurrencyType type = CurrencyType::USD)
+{
+    this->value = value;
+    this->type = type;
+}
+
+Currency::Currency(Currency& other)
+{
+    this->value = other.value;
+    this->type = other.type;
+}
+
+void Currency::ChangeType(CurrencyType type)
+{
+    this->type = type;
+}
+
+float Currency::ConvertTo(CurrencyType type) const
+{
+    return this->value * Rates[type];
+}
+
+float Currency::operator=(float value)
+{
+    return (this->value = value);
+}
+
+float Currency::operator+(float operand)
+{
+    return this->value + operand;
+}
+
+float Currency::operator-(float operand)
+{
+    return this->value - operand;
+}
+
+float Currency::operator*(float operand)
+{
+    return this->value * operand;
+}
+
+float Currency::operator/(float operand)
+{
+    /* instead of throwing errors, return obviously wrong value. */
+    if (operand == 0)
+        return 1;
+    
+    return this->value / operand;
+}
+
+std::ostream& operator<<(std::ostream& os, const Currency& data)
+{
+    os << data.value;
+
+    switch (data.type)
+    {
+    case CurrencyType::USD:
+        os << " USD";
+        break;
+    case CurrencyType::Rupee:
+        os << " Rupee";
+        break;
+    case CurrencyType::Shekel:
+        os << " Shekel";
+        break;
+    case CurrencyType::RUB:
+        os << " RUB";
+        break;
+    }
+
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, Currency& data)
+{
+    return is >> data.value;
+}
+
+Currency::operator float() const
+{
+    return this->value;
+}
---
+++ Currency.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+
+enum CurrencyType {USD, Rupee, Shekel, RUB};
+const float Rates[] = {1.0f, 82.86f, 3.44f, 64.65};
+
+/* Class for managing currency. */
+class Currency
+{
+private:
+    float value;  // amount, always given in USD
+    CurrencyType type;  // coin type, used for output formatting
+
+public:
+    Currency();
+    Currency(float value, CurrencyType type = CurrencyType::USD);
+    Currency(Currency& other);
+
+    void ChangeType(CurrencyType type);
+    float ConvertTo(CurrencyType type) const;
+
+    float operator=(float value);
+    float operator+(float operand);
+    float operator-(float operand);
+    float operator*(float operand);
+    float operator/(float operand);
+
+    explicit operator float() const;
+
+    /* allows streaming currency to output streams. */
+    friend std::ostream& operator<<(std::ostream& os, const Currency& data);
+    /* allows changing value from input streams. */
+    friend std::istream& operator>>(std::istream& is, Currency& data);
+};
