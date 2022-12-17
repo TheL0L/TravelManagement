2e126e9330eb38b926f335fa0f45a785ae5d7e36
Sergei <sergeiyakima5@gmail.com>
2022-12-17 16:34:38 +0200

    changed file structure for wider support

--- InputHandler.cpp
+++ InputHandler.cpp
@@ -1,12 +1,4 @@
-#pragma once
-#include <iostream>
-#include <limits>
-
-using std::cin;
-using std::cout;
-using std::endl;
-using std::numeric_limits;
-using std::streamsize;
+#include "InputHandler.h"
 
 /* Read a string with spaces after cin>> inputs.
 In case there were no such previous inputs,
