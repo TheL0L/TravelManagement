472015b62c6d8b652c5aceeca69ffe2f5d93a8d4
Sergei <sergeiyakima5@gmail.com>
2022-12-18 03:43:39 +0200

    expanded on setting's implementaion

---
+++ Settings.cpp
@@ -0,0 +1,38 @@
+#include "Settings.h"
+#include "FileManager.h"
+
+
+Settings::Settings()
+{
+	this->last_travel_code = 0;
+	this->default_currency = 1;
+}
+
+/* Save settings to file. */
+void Settings::LoadSettings()
+{
+	FileManager<Settings>().ReadFromFile_at_offset(_settings_filename, 0, *this);
+}
+
+/* Load settings from file. */
+void Settings::SaveSettings()
+{
+	FileManager<Settings>().InsertToFile_at_offset(_settings_filename, *this, 0);
+}
+
+/* Load ONLY travel code. */
+void Settings::LoadTravelCode()
+{
+	Settings buffer;
+	buffer.LoadSettings();
+	this->last_travel_code = buffer.last_travel_code;
+}
+
+/* Save ONLY travel code. */
+void Settings::SaveTravelCode()
+{
+	Settings buffer;
+	buffer.last_travel_code = this->last_travel_code;
+	buffer.SaveSettings();
+}
+
--- Settings.h
+++ Settings.h
@@ -1,8 +1,21 @@
 #pragma once
 
+#define _settings_filename "settings.bin"
+
+
 struct Settings
 {
-    int last_travel_code = 1;
-    int default_currency = 1;
+    int last_travel_code;
+    int default_currency;
+
+    Settings();
+    /* Save settings to file. */
+    void LoadSettings();
+    /* Load settings from file. */
+    void SaveSettings();
+    /* Load ONLY travel code. */
+    void LoadTravelCode();
+    /* Save ONLY travel code. */
+    void SaveTravelCode();
 };
 
